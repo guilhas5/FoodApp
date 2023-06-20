2875f94d07320736762951d2e9a6355b9030db00
Guilherme Costa <costaguilherme1995@gmail.com>
2023-06-20 15:23:39 +0200

    Footer implementation

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">
+                <footer>&#169; 2023 Powered by plants</footer>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
