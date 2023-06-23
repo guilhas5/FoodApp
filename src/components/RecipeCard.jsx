1d0dc833b4ccb5454cc20b05d1ea5a2ac75f904e
Guilherme Costa <costaguilherme1995@gmail.com>
2023-06-23 16:31:25 +0200

    changing the nested ternary for functions

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,12 +1,4 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-function RecipeCard({
-  searchQuery,
-  setSelectedRecipe,
-  handleFilter,
-}) {
+function RecipeCard({ searchQuery, setSelectedRecipe, handleFilter }) {
   const [recipeData, setRecipeData] = useState(null);
   const [error, setError] = useState(false);
 
@@ -28,30 +20,38 @@ function RecipeCard({
     fetchRecipeData();
   }, [searchQuery, handleFilter]);
 
+  const renderRecipeCard = () => {
+    if (error) {
+      return (
+        <h1 className="error--message">
+          We are sorry but our website is under maintenance.
+        </h1>
+      );
+    }
+
+    if (recipeData) {
+      if (recipeData.results.length > 0) {
+        return recipeData.results.map((recipe) => (
+          <div key={recipe.id} onClick={() => setSelectedRecipe(recipe)}>
+            <div className="card">
+              <h1 className="title--recipe">{recipe.title}</h1>
+              <img className="img--recipe" src={recipe.image} alt={recipe.title} />
+            </div>
+          </div>
+        ));
+      } else {
+        return (
+          <h1 className="error--message">We are sorry but we can't find a recipe.</h1>
+        );
+      }
+    }
+
+    return <h1 className="loading">Loading...</h1>;
+  };
+
   return (
-    <>
-      <div className="container--card">
-        {error ? (
-          <h1 className="error--message">We are sorry but our website is under maintenance.</h1>
-        ) : recipeData ? (
-          recipeData.results.length > 0 ? (
-            recipeData.results.map((recipe) => (
-              <div key={recipe.id} onClick={() => setSelectedRecipe(recipe)}>
-                <div className="card">
-                  <h1 className="title--recipe">{recipe.title}</h1>
-                  <img className="img--recipe" src={recipe.image} alt={recipe.title} />
-                </div>
-              </div>
-            ))
-          ) : (
-            <h1 className="error--message">We are sorry but we can't find a recipe.</h1>
-          )
-        ) : (
-          <h1 className="loading">Loading...</h1>
-        )}
-      </div>
-    </>
+    <div className="container--card">
+      {renderRecipeCard()}
+    </div>
   );
 }
-
-export default RecipeCard;
