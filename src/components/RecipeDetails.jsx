39455a5df269aba20cef0fd3135a0913f4fdfc72
Guilherme Costa <costaguilherme1995@gmail.com>
2023-06-23 16:03:49 +0200

    fixing footer

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight, faBowlFood, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faBowlFood, faClock } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 function RecipeDetails({
